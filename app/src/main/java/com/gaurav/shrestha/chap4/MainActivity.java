d3e174b2498db2591b48b3085ed1c5a84df62e2f
ShresthaGaurav <gauravshrestha202@gmail.com>
2019-10-25 10:21:58 +0545

    added ListView

--- app/src/main/java/com/gaurav/shrestha/chap4/MainActivity.java
+++ app/src/main/java/com/gaurav/shrestha/chap4/MainActivity.java
@@ -2,13 +2,20 @@ package com.gaurav.shrestha.chap4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.opengl.EGLExt;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv=findViewById(R.id.lvs);
+        String countries[]={"nepal","India","china","uk","usa","Canada"};
+        ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_expandable_list_item_1,countries);
+        lv.setAdapter(arrayAdapter);
     }
 }
